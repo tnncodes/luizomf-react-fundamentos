8b26981ed6843dffcaa69cda1e482397f302e347
tnncodes <tnncodes@gmail.com>
2022-08-13 15:54:50 -0300

    class-vs-function: manipulação do estado

--- 1-class-vs-function/src/App.js
+++ 1-class-vs-function/src/App.js
@@ -3,21 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // method bind
+    this.handleDescription = this.handleDescription.bind(this);
+
+    this.state = {
+      description: 'Edit src/App.js and save to reload.',
+      counter: 0
+    };
+  }
+
+  // método sintetico 
+  // com essa declaraçao é necessário usar um method bind
+  handleDescription () {
+    this.setState({ description: 'Edite src/App.js e salve para recarregar.' });
+  }
+
+  // com a declaração arrow function ele consegue ler o this e não é necessário o bind
+  handleLink = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+  
   render() {
+    const { description, counter } = this.state;
+
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handleDescription}>
+          {description}
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={this.handleLink}
         >
-          Learn React
+          Learn React {counter}
         </a>
       </header>
     </div>
