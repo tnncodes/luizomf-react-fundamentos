6702c754d6c9d6dd19a877aca25891cf4209bb31
tnncodes <tnncodes@gmail.com>
2022-08-20 11:39:01 -0300

    class-vs-function: configurando Mock Service Worker

--- 1-class-vs-function/src/templates/Home/index.spec.jsx
+++ 1-class-vs-function/src/templates/Home/index.spec.jsx
@@ -1,5 +1,56 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { Home } from '.';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          id: 1,
+          userId: 1,
+          title: 'title 1',
+          body: 'body 1',
+          url: 'img1.jpg',
+        },
+        {
+          id: 2,
+          userId: 2,
+          title: 'title 2',
+          body: 'body 2',
+          url: 'img2.jpg',
+        },
+        {
+          id: 3,
+          userId: 3,
+          title: 'title 3',
+          body: 'body 3',
+          url: 'img3.jpg',
+        },
+      ])
+    );
+  }),
+];
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render serach, posts and load more', async () => {
+    render(<Home />);
+    await waitForElementToBeRemoved(screen.getByText('Não existem posts!'));
   });
 });
