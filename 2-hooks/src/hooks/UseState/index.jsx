0ff3dccd997831ee40d3a29e595a87e531f63929
tnncodes <tnncodes@gmail.com>
2022-08-21 15:51:17 -0300

    hooks: useState

--- 2-hooks/src/hooks/UseState/index.jsx
+++ 2-hooks/src/hooks/UseState/index.jsx
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './styles.css';
+
 export const UseState = () => {
-  return <p>HookUseState</p>;
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+    // setCounter(counter + 1);
+
+    // setState com callBack
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  return (
+    <section className="container">
+      <h2>Cliques: {counter}</h2>
+      <img src={logo} alt="React logo" className={`logo ${reverseClass}`} />
+      <button className="button" type="button" onClick={handleClick}>
+        toggle
+      </button>
+    </section>
+  );
 };
