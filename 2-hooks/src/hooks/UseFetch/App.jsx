ff02fc7054b52277fcaa439c0a7f30602de6c64b
tnncodes <tnncodes@gmail.com>
2022-08-28 14:39:09 -0300

    hooks: atualiza - useFetch

---
+++ 2-hooks/src/hooks/UseFetch/App.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from 'react';
+import { useFetch } from './index';
+
+const App = () => {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    {
+      headers: {
+        abc: `1${postId}`,
+      },
+    }
+  );
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  if (!loading && result) {
+    // 1234
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <h1>Oi</h1>;
+};
+
+export default App;
